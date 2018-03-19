c945ae6142df50a91e8439323b86965c86958d72
蒲文学 <582725187@qq.com>
2018-03-19 11:16:08 +0800

    3，三次提交

--- Test1/app/src/main/java/com/example/puwenxue/test1/MainActivity.java
+++ Test1/app/src/main/java/com/example/puwenxue/test1/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        System.out.println("========666==555=777=======");
+        System.out.println("========666==555=777===ee====");
     }
 }
